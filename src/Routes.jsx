4f10c3cf4cb06d3db0a97fc732a3f2ad01a274a6
chrisbermejo <chrisbermejo1@gmail.com>
2023-08-18 21:43:04 -0400

    fixed routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,7 +4,6 @@ import { Routes, Route } from 'react-router-dom';
 import App from './App';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
-import RoomCreate from './pages/room'
 import RequireAuth from './components/RequireAuth';
 
 import useAuth from './hooks/useAuth';
@@ -17,7 +16,6 @@ export default function Views() {
         <Routes>
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-            <Route path='/room/create' element={<RoomCreate />} />
             <Route element={<RequireAuth />}>
                 <Route index path="/channel" element={<App />} />
             </Route>
