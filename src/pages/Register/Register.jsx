bfc8526352924f15fb68937afbbd978778e7f601
chrisbermejo <chrisbermejo1@gmail.com>
2023-08-18 23:52:06 -0400

    added useState

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useSocket from '../../hooks/useSocket';
 import useAuth from '../../hooks/useAuth';
