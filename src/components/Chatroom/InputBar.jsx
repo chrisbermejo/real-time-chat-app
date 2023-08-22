4fd8bdea9e59ffd4216fd931fcb75f8136ab73dd
chrisbermejo <chrisbermejo1@gmail.com>
2023-08-22 00:47:42 -0400

    downgraded input, better one soon.

--- src/components/Chatroom/InputBar.jsx
+++ src/components/Chatroom/InputBar.jsx
@@ -5,21 +5,28 @@ export function ChatRoomInputBar() {
     const { sendMessage, message, setMessage } = useInfo();
 
     const handleKeyPress = (e) => {
-        if (e.key === 'Enter' && e.shiftKey) {
-            e.target.rows = e.target.rows + 1;
-        }
+        // if (e.key === 'Enter' && e.shiftKey) {
+        //     e.target.rows = e.target.rows + 1;
+        // }
         if (e.key === 'Enter' && !e.shiftKey) {
             e.preventDefault();
             sendMessage();
-            e.target.rows = 1;
             setMessage('');
         }
     };
 
     return (
         <div className='chatroom-inputs-container'>
-            <textarea
+            {/* <textarea
                 rows={1}
+                className='chatroom-input'
+                type="text"
+                placeholder="Message..."
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyPress={handleKeyPress}
+            /> */}
+            <input
                 className='chatroom-input'
                 type="text"
                 placeholder="Message..."
